94a93575c8e59fa8c911bcaa243abdd23f447b64
Ueslei09 <uesleirome@gmail.com>
2026-06-21 13:03:55 -0300

    atualizando db

--- TerinandoVue/src/App.vue
+++ TerinandoVue/src/App.vue
@@ -4,3 +4,25 @@
  </div>
 
 </template>
+//import { ref, onMounted } from 'vue';
+<script>
+ import { ref, onMounted } from 'vue';
+ //import { ApiMEuBanco } from './serviceApi/ApiMeuBanco.js';
+ import{ApiMEuBanco} from './serviceApi/ApiMeuBanco.js';
+
+ const ref =([]);
+
+ const buscarDados = async () => {
+  try {
+   const response = await ApiMEuBanco.get('/dados');
+   ref.value = response.data;
+  } catch (error) {
+   console.error('Erro ao buscar dados:', error);
+  }
+ };
+  onMounted(() => {
+    buscarDados();
+  });
+
+
+</script>
